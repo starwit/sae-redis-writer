7cf1be3ea845dd80ddb0ab085b33297c227bc2d1
flonix8 <flstanek@googlemail.com>
2024-05-23 15:53:41 +0200

    Version 1.2.0: Use pipelined redis publisher to increase performance on higher RTT links (max batch size = buffer length)

--- rediswriter/config.py
+++ rediswriter/config.py
@@ -1,20 +1,22 @@
-from pydantic import BaseModel, conint, conlist
-from pydantic_settings import BaseSettings, SettingsConfigDict
-from visionlib.pipeline.settings import LogLevel, YamlConfigSettingsSource
 from typing import List
+
+from pydantic import BaseModel, Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing_extensions import Annotated
+from visionlib.pipeline.settings import LogLevel, YamlConfigSettingsSource
 
 
 class RedisConfig(BaseModel):
     host: str = 'localhost'
-    port: conint(ge=1, le=65536) = 6379
+    port: Annotated[int, Field(ge=1, le=65536)] = 6379
     input_stream_prefix: str = 'objecttracker'
 
 class TargetRedisConfig(BaseModel):
     host: str
-    port: conint(ge=1, le=65536)
+    port: Annotated[int, Field(ge=1, le=65536)]
     output_stream_prefix: str = 'output'
-    buffer_length: conint(ge=1) = 10
-    target_stream_maxlen: conint(ge=1) = 100
+    buffer_length: Annotated[int, Field(ge=1)] = 100
+    target_stream_maxlen: Annotated[int, Field(ge=1)] = 100
     tls: bool = False
 
 class RedisWriterConfig(BaseSettings):
--- rediswriter/sender.py
+++ rediswriter/sender.py
@@ -2,13 +2,13 @@ import logging
 import time
 from collections import deque
 from threading import Event, Thread
-from typing import Deque, NamedTuple
+from typing import Deque, List, NamedTuple, Tuple
 
 import backoff
 from log_rate_limit import StreamRateLimitFilter
-from prometheus_client import Counter, Histogram
+from prometheus_client import Counter, Histogram, Summary
 from redis.exceptions import ConnectionError, TimeoutError
-from visionlib.pipeline.publisher import RedisPublisher
+from visionlib.pipeline.publisher import RedisPipelinePublisher
 
 from .config import RedisWriterConfig
 
@@ -21,6 +21,7 @@ GIVEUP_COUNTER = Counter('redis_writer_giveup_counter', 'How many messages were 
 DISCARD_BUFFER_COUNTER = Counter('redis_writer_discard_buffer_counter', 'How many input messages have to be discarded because sender cannot keep up')
 REDIS_PUBLISH_DURATION = Histogram('redis_writer_target_redis_publish_duration', 'The time it takes to push a message onto the Redis stream',
                                    buckets=(0.0025, 0.005, 0.0075, 0.01, 0.025, 0.05, 0.075, 0.1, 0.15, 0.2, 0.25))
+REDIS_PUBLISH_BYTES_SENT = Summary('redis_writer_target_redis_published_bytes_estimate', 'How many bytes were sent to the Redis stream (this is estimated!)')
 
 def _on_backoff(_):
     logger.debug(f'Failed to send message. Backing off...')
@@ -63,7 +64,7 @@ class Sender:
         return self._publish
     
     def _run(self):
-        publisher = RedisPublisher(
+        publisher = RedisPipelinePublisher(
             host=self._config.host,
             port=self._config.port,
             stream_maxlen=self._config.target_stream_maxlen,
@@ -72,17 +73,34 @@ class Sender:
 
         with publisher as publish:
             while not self._stop_event.is_set():
-                try:
-                    entry = self._buffer.popleft()
-                except IndexError:
+                batch = self._get_next_batch()
+                    
+                if len(batch) == 0:
                     time.sleep(0.05)
                     continue
 
                 try:
-                    self._publish_with_backoff(publish, entry.stream_key, entry.msg_bytes)
+                    self._publish_with_backoff(publish, batch)
                 except (ConnectionError, TimeoutError) as e:
                     logger.error(f'Gave up sending message', exc_info=True)
                     GIVEUP_COUNTER.inc()
+
+    def _get_next_batch(self):
+        batch = []
+        batch_bytes = 0
+        try:
+            while len(batch) < self._config.buffer_length: 
+                entry = self._buffer.popleft()
+                batch.append(entry)
+                # Assume 33% overhead for b64 encoding
+                batch_bytes += round(len(entry.msg_bytes) * 1.33) + len(entry.stream_key)
+        except IndexError:
+            pass
+
+        if len(batch) > 0:
+            REDIS_PUBLISH_BYTES_SENT.observe(batch_bytes)
+
+        return batch
 
     @backoff.on_exception(
             backoff.expo, 
@@ -93,9 +111,9 @@ class Sender:
             factor=0.1, 
             max_value=5,
     )
-    def _publish_with_backoff(self, publish: callable, stream_key: str, msg_bytes: bytes):
+    def _publish_with_backoff(self, publish: callable, stream_entries: List[Tuple[str, bytes]]):
         with REDIS_PUBLISH_DURATION.time():
-            publish(stream_key, msg_bytes)
+            publish(stream_entries)
 
     def __exit__(self, _, __, ___):
         self._stop_event.set()
